82240131d78f845fdd4e3e28768f82ab91fcb5d8
Abhishek <abhishek.bisht54@yahoo.com>
2022-01-27 11:31:35 +0530

    File Organization

--- FileSystemOrganiser/main.js
+++ FileSystemOrganiser/main.js
@@ -74,15 +74,18 @@ function organizeHelper(src,dest){
 	let childNames = fs.readdirSync(src);
 	// console.log(childNames);
 	for(let i=0;i<childNames.length;i++){
+		// Creating the address for each file.
 		let childAddress = path.join(src,childNames[i]);
 
 		// check if it is file or folder
 		let isFile = fs.lstatSync(childAddress).isFile();
 		if(isFile){
 			// console.log(childNames[i]);
-			
+
+			// To identify to which category does the file belong to
 			let category = getCategory(childNames[i]);
-			console.log(childNames[i], "belongs to ---> ", category);
+
+			// console.log(childNames[i], "belongs to ---> ", category);
 
 			// 4) copy/cut files to that organized directory inside any of the category folder.
 			sendFiles(childAddress,dest,category);
@@ -93,10 +96,11 @@ function organizeHelper(src,dest){
 
 function getCategory(name){
 let ext = path.extname(name);
-ext = ext.slice(1);
+ext = ext.slice(1); // seperating the dot from the extension name
 
+// Searchinh in the types object
 for(let type in types){
-	let currtypeArray = types[type];
+	let currtypeArray = types[type]; // -> currTypeArray can have values => media, archives,document,app
 	for(let i=0;i<currtypeArray.length;i++){
 		if(ext == currtypeArray[i]){
 			return type;
@@ -114,12 +118,13 @@ function sendFiles(srcFilePath, dest, category){
 
 	let categoryPath = path.join(dest, category);
 	if(fs.existsSync(categoryPath) == false){
-		fs.mkdirSync(categoryPath);
+		fs.mkdirSync(categoryPath); // Creating a file specifically for a extension type
 	}
 
 	let fileName = path.basename(srcFilePath);
 	let destFilePath = path.join(categoryPath,fileName)
-	fs.copyFileSync(srcFilePath,destFilePath)
+	fs.copyFileSync(srcFilePath,destFilePath) // copying the files into destFilePath
+	fs.unlinkSync(srcFilePath) // deleting the copied files. To give a feel of cutting the pasting.
 
 }
 
