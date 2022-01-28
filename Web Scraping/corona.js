e5dcb9dab9d96bb13b77f8c227bd61356359a1af
Abhishek <abhishek.bisht54@yahoo.com>
2022-01-28 13:14:33 +0530

    Web Scraping Worldometer corona virus info

--- Web Scraping/corona.js
+++ Web Scraping/corona.js
@@ -1,4 +1,7 @@
 const request = require('request');
+const cheerio  = require('cheerio');
+
+
 
 console.log("before")
 request('https://www.worldometers.info/coronavirus/', cb)
@@ -9,12 +12,36 @@ console.log("after")
 
 function cb(error, response, html) {
 
-	if (err) {
+	if (error) {
 		console.error('error:', error); // Print the error if one occurred
 
 	} else {
-		console.log('html:', html); // Print the HTML for the Google homepage.
+		
+		handleHtml(html);
 
 	}
+
+}
+
+
+function handleHtml(html){
+	let selectorTool  = cheerio.load(html);
+	// let h1s = selectorTool("h1");
+	// console.log(h1s.length);
+
+	let ContentArr = selectorTool("#maincounter-wrap span");
+	// for(let i=0;i<ContentArr.length;i++){
+	// 	let data =  selectorTool(ContentArr[i]).text();
+	// 	console.log(data);
+	// }
+
+	let total_Cases = selectorTool(ContentArr[0]).text();
+	let total_Death = selectorTool(ContentArr[1]).text();
+	let total_Recovered = selectorTool(ContentArr[2]).text();
+
+	console.log("Total Cases -> " + total_Cases)
+	console.log("Total Deaths -> " + total_Death)
+	console.log("Total Recovered -> " + total_Recovered)
+
 
 }
