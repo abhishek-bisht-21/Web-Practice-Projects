5cba46dd00be437a996f6d5ac1ed3911017ad0c8
Abhishek <abhishek.bisht54@yahoo.com>
2022-01-28 13:17:48 +0530

    Web Scraping Worldometer corona virus info

--- Web Scraping/corona.js
+++ Web Scraping/corona.js
@@ -4,6 +4,7 @@ const cheerio  = require('cheerio');
 
 
 console.log("before")
+
 request('https://www.worldometers.info/coronavirus/', cb)
 
 console.log("after")
@@ -23,18 +24,23 @@ function cb(error, response, html) {
 
 }
 
-
+// Callback function
 function handleHtml(html){
+
+	// Just CSS Selector Cheerio also has selectors to select specific html from the page
 	let selectorTool  = cheerio.load(html);
 	// let h1s = selectorTool("h1");
 	// console.log(h1s.length);
 
+	// After finding out the specific selectors, having specific info
 	let ContentArr = selectorTool("#maincounter-wrap span");
+
 	// for(let i=0;i<ContentArr.length;i++){
 	// 	let data =  selectorTool(ContentArr[i]).text();
 	// 	console.log(data);
 	// }
 
+	// It is neccessary to again wrap it with selectorTool to get the desired text
 	let total_Cases = selectorTool(ContentArr[0]).text();
 	let total_Death = selectorTool(ContentArr[1]).text();
 	let total_Recovered = selectorTool(ContentArr[2]).text();
